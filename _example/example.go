ec46a79d42db13a5d2b33a1744895112069312c1
mattn <mattn.jp@gmail.com>
2014-08-25 22:47:08 +0900

    Remove old files

--- _example/example.go
+++ _example/example.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"time"
 )
 
@@ -12,17 +13,32 @@ func main() {
 	filename := "foo.log"
 	sw := sw.NewWriter(filename, 500, 0644, func(sw *sw.Writer) error {
 		var newname string
+		ext := filepath.Ext(sw.Filename)
+		base := filename[:len(filename)-len(ext)]
 		for {
-			ext := filepath.Ext(sw.Filename)
-			newname = filename[:len(filename)-len(ext)] + "-" + time.Now().Format("20060102150405") + ext
+			newname = base + "-" + time.Now().Format("20060102150405") + ext
 			_, err := os.Lstat(newname)
 			if os.IsNotExist(err) {
 				break
 			}
 			time.Sleep(1 * time.Millisecond)
 		}
+		err := os.Rename(filename, newname)
+		if err != nil {
+			return err
+		}
 		println("rotate", filename, newname)
-		return os.Rename(filename, newname)
+
+		files, err := filepath.Glob(base + "-*" + ext)
+		if err != nil {
+			return err
+		}
+		if len(files) > 5 {
+			sort.Strings(files)
+			println("delete", files[0])
+			return os.Remove(files[0])
+		}
+		return nil
 	})
 
 	log.SetOutput(sw)
